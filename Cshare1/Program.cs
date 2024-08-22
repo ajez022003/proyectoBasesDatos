114a5e07faddc63fc96a30f63a6a0b033ba48960
ajez022003 <angellyjessibelelvir@gmail.com>
2024-08-22 00:45:37 -0600

    adelanto con procedimiento

---
+++ Cshare1/Program.cs
@@ -0,0 +1,23 @@
+namespace Cshare1
+{
+    internal static class Program
+    {
+
+
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            string Nombre = "Eduardo";
+            MessageBox.Show(Nombre);
+            //// To customize application configuration such as set high DPI settings or default font,
+            //// see https://aka.ms/applicationconfiguration.
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new Form1());
+        }
+    }
+}
